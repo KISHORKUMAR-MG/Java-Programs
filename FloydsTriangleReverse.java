bba4183fa04ca6fe6f7a8bb800359d8f0e444dc9
M G Kishor Kumar <163229267+KISHORKUMAR-MG@users.noreply.github.com>
2025-12-18 14:39:03 +0530

    Rename class Fl to FloydsTriangleReverse

---
+++ FloydsTriangleReverse.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class FloydsTriangleReverse{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int num=n*(n+1)/2;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(num+" ");
+                num--;
+            }
+            System.out.println();
+        }
+    }
+}
