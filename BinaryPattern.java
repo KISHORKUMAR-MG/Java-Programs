c729fd29119a8a2920473a1f416bcc7c76a3de5e
M G Kishor Kumar <163229267+KISHORKUMAR-MG@users.noreply.github.com>
2025-12-18 14:58:39 +0530

    Create BinaryPattern.java

---
+++ BinaryPattern.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class BinaryPattern{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            int num;
+            if(i%2!=0){
+                num=1;
+            }
+            else{
+                num=0;
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print(num);
+                num=1-num;
+            }
+            System.out.println();
+        }
+    }
+}
