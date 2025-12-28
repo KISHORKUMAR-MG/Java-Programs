625752b6e2d4b8388ca5c37a14999d27b50e4ca3
M G Kishor Kumar <163229267+KISHORKUMAR-MG@users.noreply.github.com>
2025-12-28 13:12:33 +0530

    Add SortedOrNot class to check the array is sorted or not

---
+++ Array/SortedOrNOt.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class SortedOrNot{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        boolean isSorted=true;
+        for(int i=0;i<n-1;i++){
+            if(arr[i]>arr[i+1]){
+                isSorted=false;
+                break;
+            }
+        }
+        if(isSorted==true){
+            System.out.print("Sorted");
+        }
+        else{
+            System.out.print("Not Sorted");
+        }
+    }
+}
